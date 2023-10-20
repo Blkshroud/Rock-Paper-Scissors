a14323fdcff9abc829d0ee8be0011eee05d3beec
Blkshroud <145509002+Blkshroud@users.noreply.github.com>
2023-10-20 19:21:45 -0400

    Created comparison function to decide winner Console messages have been added through for easy troubleshooting and will be removed after fully functioning app is completed. Player function needs updating to not accept answers other than rock, paper, or scissors (case doesn't matter though);

--- javascript.js
+++ javascript.js
@@ -1,12 +1,16 @@
 
 function getComputerChoice(){
     let numSel = Math.floor(Math.random() * 3);
+    console.log(numSel);
     switch (numSel) {
         case 0:
+            console.log("rock");
             return 'rock';
         case 1:
+            console.log("paper");
             return 'paper';
         case 2:
+            console.log("scissors");
             return 'scissors';
     }
 }
@@ -17,5 +21,21 @@ function getPlayerChoice() {
     return pChoice;
 }
 
-console.log(getComputerChoice());
-getPlayerChoice();
+function whoWins(){
+    let pChoice = getPlayerChoice();
+    let cChoice = getComputerChoice();
+    if ((pChoice === "rock" && cChoice === "scissors") || (pChoice === "paper" && cChoice === "rock") || (pChoice === "scissors" && cChoice === "paper")){
+        console.log("pWin");
+        return "pWin";
+    }
+    else if((cChoice === "rock" && pChoice === "scissors") || (cChoice === "paper" && pChoice === "rock") || (cChoice === "scissors" && pChoice === "paper")){
+        console.log("cWin");
+        return "cWin";
+    }
+    else{
+        console.log("tie");
+        return "tie";
+    }
+}
+
+whoWins();
