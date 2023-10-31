94ac44c878def42184467eb31a3a3cdc62df2429
Blkshroud <145509002+Blkshroud@users.noreply.github.com>
2023-10-31 14:38:49 -0400

    Modified JS to allow play through UI Consider beautifying the visuals before merging into main branch.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,43 @@
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorBtn = document.querySelector('#scissors');
+
+function rock(){
+    playRound('rock');
+}
+function paper(){
+    playRound('paper');
+}
+function scissors(){
+    playRound('scissors');
+}
+
+rockBtn.addEventListener('click', rock);
+paperBtn.addEventListener('click', paper);
+scissorBtn.addEventListener('click', scissors);
+
+const container = document.querySelector("div");
+
+const pWinsText = document.createElement("p");
+const cWinsText = document.createElement("p");
+const currentWinnerText = document.createElement("p");
+const overallWinnerText = document.createElement("p");
+
+container.appendChild(pWinsText);
+container.appendChild(cWinsText);
+container.appendChild(currentWinnerText);
+container.appendChild(overallWinnerText);
+let pWins = 0;
+let cWins = 0;
+let matchCount = 0;
+
+const restartBtn = document.createElement("button");
+restartBtn.textContent = 'Play Again';
+
+
+pWinsText.textContent = `You have won ${pWins} time(s).`;
+cWinsText.textContent = `The computer has won ${cWins} time(s).`;
+currentWinnerText.textContent = `Click a button to start playing!`;
 
 function getComputerChoice(){ //selects a number from 0-2 randomly and converts to string with switch statement
     let numSel = Math.floor(Math.random() * 3);
@@ -14,70 +54,62 @@ function getComputerChoice(){ //selects a number from 0-2 randomly and converts 
     }
 }
 
-function getPlayerChoice() { // Asks the player for their entry and verifies that it is a playable entry.
-    let pChoice = prompt("Rock, paper, or scissors?");
-    if(pChoice === null){ // Checks nulls from esc before proceeding to make case insensitive and verify entry.
-        alert(`Answer must be rock, paper, or scissors! You didn't enter anything!`);
-         // re-runs the function when an unuseable answer is given
-        return getPlayerChoice();
-    }
-    else{
-        pChoice = pChoice.toLowerCase(); // makes the check case-insensitive
-        if(pChoice === 'rock' || pChoice === 'paper' || pChoice === 'scissors'){
-            return pChoice;
-        }
-        else{
-            alert(`Answer must be rock, paper, or scissors! You entered ${pChoice}`);
-             // re-runs the function when an unuseable answer is given
-            return getPlayerChoice();
-        }   
-    }
-     
-}
-
-function playRound(){
-    let pChoice = getPlayerChoice();
+function playRound(pChoice){
     console.log(`Player ${pChoice}`);
     let cChoice = getComputerChoice();
     if ((pChoice === "rock" && cChoice === "scissors") || (pChoice === "paper" && cChoice === "rock") || (pChoice === "scissors" && cChoice === "paper")){
+        currentWinnerText.textContent = `You win! ${pChoice} beats ${cChoice}.`;
+        matchCount++;
+        pWins++;
+        pWinsText.textContent = `You have won ${pWins} time(s).`;
+        if(pWins === 5){
+            overallWinnerText.textContent = `You are the overall victor! You won ${pWins} times and the cpu won ${cWins} times. It took a total of ${matchCount} rounds for a winner to be decided.`
+            container.appendChild(overallWinnerText);
+            container.appendChild(restartBtn);
+            restartBtn.addEventListener('click', resetGame);
+            rockBtn.removeEventListener('click', rock);
+            paperBtn.removeEventListener('click', paper);
+            scissorBtn.removeEventListener('click', scissors);
+            return console.log("Game Complete");
+        }
         return `You win! ${pChoice} beats ${cChoice}.`;
     }
     else if((cChoice === "rock" && pChoice === "scissors") || (cChoice === "paper" && pChoice === "rock") || (cChoice === "scissors" && pChoice === "paper")){
+        currentWinnerText.textContent = `You lose! ${cChoice} beats ${pChoice}.`;
+        matchCount++;
+        cWins++;
+        cWinsText.textContent = `The computer has won ${cWins} time(s).`;
+        if(cWins === 5){
+            overallWinnerText.textContent = `The CPU is the overall victor. You won ${pWins} times before the cpu won ${cWins} times. It took a total of ${matchCount} rounds for a winner to be decided.`;
+            container.appendChild(overallWinnerText);
+            container.appendChild(restartBtn);
+            restartBtn.addEventListener('click', resetGame);
+            rockBtn.removeEventListener('click', rock);
+            paperBtn.removeEventListener('click', paper);
+            scissorBtn.removeEventListener('click', scissors);
+            return console.log("Game Complete");
+        }
         return `You lose! ${cChoice} beats ${pChoice}.`;
     }
     else{
+        currentWinnerText.textContent = `It was a tie. You both played ${cChoice}. Try again!`;
+        matchCount++;
         return `It was a tie. You both played ${cChoice}. Try again!`;
     }
 }
 
-function game(){
-    let pWins = 0;
-    let cWins = 0;
-    let matchCount = 0;
-    while(pWins < 3 && cWins < 3){
-        let result = playRound();
-        let winner = result.substring(0,5)
-        if(winner === `You w`){
-            pWins++;
-            console.log(result);
-            matchCount++;
-        }
-        else if(winner === `You l`){
-            cWins++;
-            console.log(result);
-            matchCount++;
-        }
-        else{
-            console.log(result);
-            matchCount++;
-        }
-    }
-    if(pWins > cWins){
-        console.log(`You are the overall victor! You won 3 times and the cpu won ${cWins} times. It took a total of ${matchCount} rounds for a winner to be decided.`);
-    }
-    else{
-        console.log(`The CPU is the overall victor. You won ${pWins} times before the cpu won ${cWins} times. It took a total of ${matchCount} rounds for a winner to be decided.`);
-    }
-}
 
-game();
+function resetGame(){
+    pWins = 0;
+    cWins = 0;
+    matchCount = 0;
+    restartBtn.removeEventListener('click', resetGame);
+    container.removeChild(restartBtn);
+    container.removeChild(overallWinnerText);
+    pWinsText.textContent = `You have won ${pWins} time(s).`;
+    cWinsText.textContent = `The computer has won ${cWins} time(s).`;
+    currentWinnerText.textContent = `Click a button to start playing!`;
+    rockBtn.addEventListener('click', rock);
+    paperBtn.addEventListener('click', paper);
+    scissorBtn.addEventListener('click', scissors);
+}
